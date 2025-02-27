f253ad0111731c6cbc79b0ffc9933d9db1c1ad57
Solar138 <46548002+solar138@users.noreply.github.com>
2025-02-26 20:16:12 -0800

    Events log and buttons

--- main.js
+++ main.js
@@ -2,12 +2,16 @@ if (!localStorage) {
   alert("No localStorage available, data may be lost.");
 } 
 
+const cageStates = ["No Attempt", "Shallow Climb", "Deep Climb"];
+
 class ScoutingData {
   constructor(roundNumber, teamNumber, scouterName, alliance) {
     this.roundNumber = roundNumber;
     this.teamNumber = teamNumber;
     this.alliance = alliance;
     this.scouterName = scouterName;
+    this.cage = cageStates[0];
+    this.events = [];
   }
 }
 
@@ -25,8 +29,6 @@ if (data.length == 0) {
   data[0] = currentData;
 }
 
-const allianceToggle = document.getElementById("allianceToggle");
-
 teamNumber.value = currentData.teamNumber;
 roundNumber.value = currentData.roundNumber;
 scouterName.value = currentData.scouterName;
@@ -41,10 +43,7 @@ if (currentData.alliance == "blue") {
 
 document.getElementById("submit").disabled = !validateData();
 
-var bargeStates = ["No Attempt", "Robot Shoot", "Human Throw"];
-var cageStates = ["No Attempt", "Shallow Climb", "Deep Climb"];
-
-function toggleStates(states, button, key, prefix) {
+function stateButton(states, button, key, prefix) {
 
   button.addEventListener("click", e => {
     var index = (states.indexOf(currentData[key] ?? states[0]) + 1) % states.length;
@@ -57,14 +56,65 @@ function toggleStates(states, button, key, prefix) {
   button.innerHTML = prefix + ":<br>" + currentData[key] ?? states[0];
 }
 
+function eventButton(button, name) {
+
+  button.addEventListener("click", e => {
+    var event = {
+      name: name,
+      time: Date.now() - gameStartTime,
+    };
+
+    if (!currentData.events) {
+      currentData.events = [];
+    }
+    currentData.events.push(event);
+    createLogEntry(event.time, name, () => {
+      currentData.events.splice(currentData.events.indexOf(event), 1);
+    });
+    saveData();
+  });
+}
+
+var index = 0;
+
+function createLogEntry(time, name, undo) {
+  var li = document.createElement("li");
+  li.id = `logEntry${index}`;
+  var button = document.createElement("button");
+  button.id = `logEntry${index++}Undo`;
+  button.innerText = "UNDO";
+  button.addEventListener("click", e => {
+    log.removeChild(li);
+    undo();
+    saveData();
+  })
+  li.appendChild(button);
+  var span = document.createElement("span");
+  span.innerText = ` ${name} @ ${parseTime(time)}`;
+  li.appendChild(span);
+  log.insertBefore(li, log.firstChild);
+}
+
 document.addEventListener("DOMContentLoaded", () => {
 
   if (localStorage.currentPage) {
     loadPage(localStorage.currentPage);
   }
 
-  toggleStates(bargeStates, bargeToggle, "barge", "Barge Net");
-  toggleStates(cageStates, cageToggle, "cage", "Cage Climb");
+  for (var event of currentData.events) {
+    const e = event;
+    createLogEntry(e.time, e.name, () => {
+      currentData.events.splice(currentData.events.indexOf(e), 1);
+      saveData();
+    });
+  }
+
+  eventButton(coralIntakeButton, "Coral Intaken");
+  eventButton(processorScoreButton, "Processor Scored");
+  eventButton(bargeHumanButton, "Barge by Human");
+  eventButton(bargeRobotButton, "Barge by Robot");
+
+  stateButton(cageStates, cageToggle, "cage", "Cage Climb");
 
   allianceToggle.addEventListener("click", e => {
     if (currentData.alliance == "red") {
@@ -110,10 +160,14 @@ function validateData() {
   return currentData.teamNumber && currentData.alliance && currentData.roundNumber && currentData.scouterName;
 }
 
+function parseTime(gameTime) {
+  return Math.floor(gameTime / 60000) + ":" + Math.floor(gameTime % 60000 / 1000).toString().padStart(2, "0") + "." + Math.floor(gameTime % 1000 / 10).toString().padStart(2, "0");
+}
+
 function gameFrameUpdate() {
   if (gameStartTime) {
     var gameTime = Date.now() - gameStartTime;
-    const time = Math.floor(gameTime / 60000) + ":" + Math.floor(gameTime % 60000 / 1000).toString().padStart(2, "0") + "." + Math.floor(gameTime % 1000 / 10).toString().padStart(2, "0");
+    const time = parseTime(gameTime);
     timer.textContent = time;
     gameStatus.textContent = gameTime < autoLength * 1000 ? "Auto" : "TeleOp";
 
